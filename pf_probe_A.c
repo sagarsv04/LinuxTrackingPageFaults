b7d053151d66199c1a3eb49592a1787c73e4ded3
sagarsv04 <scoutsv.sv@gmail.com>
2020-04-29 21:32:02 -0400

    - created a buffer for page fault - created a char driver for user to read buffer

--- pf_probe_A.c
+++ pf_probe_A.c
@@ -10,24 +10,143 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/kprobes.h>
+#include <linux/fs.h>
+#include <linux/uaccess.h>
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Sagar Vishwakarma");
 MODULE_DESCRIPTION("A Simple Linux Page Faults Tracking Device");
 MODULE_VERSION("1.0");
 
-#define DRIVER_DEBUG 0
+#define PROBE_DEBUG 0
 
-#define DRIVER_NAME "pf_probe_A"
+#define PROBE_NAME "pf_probe_a"
 
+#define PROBE_STR_LEN 128
+#define PROBE_BUFFER_SIZE	500
 #define MAX_SYMBOL_LEN	64
-static char symbol[MAX_SYMBOL_LEN] = "_do_fork";
+// static char symbol[MAX_SYMBOL_LEN] = "_do_fork";
+static char symbol[MAX_SYMBOL_LEN] = "pf_probe_a";
+
+static pid_t process_id = -1;
+// static int probe_buffer_counter = 0;
+// static int probe_position = 0;
+static int probe_open_counter = 0;
+static int major_number;
+
+typedef struct page_fault_data {
+	unsigned long address;
+	long time;
+} page_fault_data;
+
+
+module_param(process_id, int, 0);
+// module_param_string(PROBE_NAME, PROBE_NAME, sizeof(PROBE_NAME), 0644);
 module_param_string(symbol, symbol, sizeof(symbol), 0644);
 
+static page_fault_data page_fault_data_buffer[PROBE_BUFFER_SIZE];
+
+
+
+/* Function Declarations */
+static int handler_pre(struct kprobe *, struct pt_regs *);
+static void handler_post(struct kprobe *, struct pt_regs *, unsigned long);
+static int handler_fault(struct kprobe *, struct pt_regs *, int);
+
+
+static int dev_open(struct inode *, struct file *);
+static ssize_t dev_read(struct file *, char *, size_t, loff_t *);
+static int dev_close(struct inode *, struct file *);
+
+static void get_fault_info(char *, loff_t *);
+
+
 /* For each probe you need to allocate a kprobe structure */
-static struct kprobe kp = {
-	.symbol_name	= symbol,
+static struct kprobe dev_kp = {
+	.symbol_name	= PROBE_NAME,
+	.pre_handler = handler_pre,
+	.post_handler = handler_post,
+	.fault_handler = handler_fault,
 };
+
+
+static struct file_operations dev_file_op = {
+	.owner		= THIS_MODULE,
+	.open			= dev_open,
+	.read			= dev_read,
+	.release	= dev_close,
+};
+
+
+/* Pass fault Info based on how many lines already read */
+static void get_fault_info(char *message, loff_t *offset) {
+
+	int skip_node = (int)(*offset);
+
+	if (skip_node >= PROBE_BUFFER_SIZE) {
+		if (PROBE_DEBUG) {
+			printk(KERN_ALERT "DEV Module: Read All Buffer Entry\n");
+		}
+		strcpy(message, "EXIT_CODE\n");
+	}
+	else {
+		// strcpy(message, page_fault_data_buffer[skip_node])
+		sprintf(message, "PID = %8d  Page Fault at Address 0x%lx at Time %ld\n", process_id, page_fault_data_buffer[skip_node].address, page_fault_data_buffer[skip_node].time);
+		*offset += 1;
+	}
+}
+
+
+/* file_operations open implementation */
+static int dev_open(struct inode *pinode, struct file *pfile) {
+
+	pid_t pid;
+	struct task_struct *task = current;
+	pid = task->pid;
+	probe_open_counter += 1;
+	printk(KERN_INFO "DEV Module: Process %d has called %s function of Dev Page Fault Driver\n", pid, __FUNCTION__);
+	if (PROBE_DEBUG) {
+		printk(KERN_INFO "DEV Module: Device opened %d Times\n", probe_open_counter);
+	}
+	return 0;
+}
+
+
+/* file_operations close implementation */
+static int dev_close(struct inode *pinode, struct file *pfile) {
+
+	pid_t pid;
+	struct task_struct *task = current;
+	pid = task->pid;
+	probe_open_counter -= 1;
+	printk(KERN_INFO "DEV Module: Process %d has called %s function of Dev Page Fault Driver\n", pid, __FUNCTION__);
+	if (PROBE_DEBUG) {
+		printk(KERN_INFO "DEV Module: Device opened %d Times\n", probe_open_counter);
+	}
+	return 0;
+}
+
+
+/* file_operations read implementation, copy info to user space */
+static ssize_t dev_read(struct file *pfile, char __user *buffer, size_t length, loff_t *offset) {
+
+	int errors = 0;
+	char message[PROBE_STR_LEN];
+	int message_len = 0;
+	pid_t pid;
+
+	pid = current->pid;
+	get_fault_info(message, offset);
+	message_len = strlen(message);
+	errors = copy_to_user(buffer, message, message_len);
+	if (PROBE_DEBUG) {
+		printk(KERN_INFO "DEV Module: Process %d has called %s function with Offset %lld\n", pid, __FUNCTION__, *offset);
+		if (errors != 0) {
+			printk(KERN_INFO "DEV Module: Failed to Copy Fault Info to Process %d with Offset %lld\n", pid, *offset);
+		}
+	}
+	return errors == 0 ? message_len : -EFAULT;
+}
 
 
 /* kprobe pre_handler: called just before the probed instruction is executed */
@@ -108,19 +227,24 @@ static int handler_fault(struct kprobe *p, struct pt_regs *regs, int trapnr) {
 static int __init pf_probe_init(void) {
 
 	int ret;
-	kp.pre_handler = handler_pre;
-	kp.post_handler = handler_post;
-	kp.fault_handler = handler_fault;
 
-	ret = register_kprobe(&kp);
+	major_number = register_chrdev(0, PROBE_NAME, &dev_file_op);
+	if (major_number < 0) {
+		printk(KERN_ALERT "DEV Module: Failed to register a major number\n");
+		return major_number;
+	}
+	else {
+		printk(KERN_INFO "DEV Module: Dev Page Fault Driver Registered with major number %d\n", major_number);
+	}
 
+	ret = register_kprobe(&dev_kp);
 	if (ret < 0) {
-		pr_err("register_kprobe failed, returned %d\n", ret);
+		printk(KERN_ALERT "DEV Module: Register KPROBE Failed Return Code %d\n", ret);
 		return ret;
 	}
-	pr_info("Planted kprobe at %p\n", kp.addr);
-	if (DRIVER_DEBUG) {
-		printk(KERN_INFO "%s Module: Installed ...\n", DRIVER_NAME);
+	printk(KERN_ALERT "DEV Module: Registered KPROBE for PID %d at Address %p\n", process_id, dev_kp.addr);
+	if (PROBE_DEBUG) {
+		printk(KERN_INFO "DEV Module: %s Installed ...\n", PROBE_NAME);
 	}
 	return 0;
 }
@@ -128,10 +252,11 @@ static int __init pf_probe_init(void) {
 
 static void __exit pf_probe_exit(void) {
 
-	unregister_kprobe(&kp);
-	pr_info("kprobe at %p unregistered\n", kp.addr);
-	if (DRIVER_DEBUG) {
-		printk(KERN_INFO "%s Module: Removed ...\n", DRIVER_NAME);
+	unregister_chrdev(major_number, PROBE_NAME);
+	unregister_kprobe(&dev_kp);
+	pr_info("kprobe at %p unregistered\n", dev_kp.addr);
+	if (PROBE_DEBUG) {
+		printk(KERN_INFO "%s Module: Removed ...\n", PROBE_NAME);
 	}
 }
 
